95e80a7c33da1cf14fe3be6d28e93ed82950719a
gemdoq <gemdoq@gmail.com>
2024-11-21 11:59:06 +0900

    initial commit

---
+++ src/main/java/com/springboot/springintegrationnumberfilter/SpringIntegrationNumberFilterApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springintegrationnumberfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationNumberFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationNumberFilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/springintegrationnumberfilter/SpringIntegrationNumberFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springintegrationnumberfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationNumberFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
